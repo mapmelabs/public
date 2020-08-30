f22fd34f68d2291bccce2755f2b696af6d6fd9d4
philippe18 <philippe@mapme.com>
2020-08-30 13:57:22 +0300

    Create google-form-submissions-to-mapme-multiple-description-fields.js

---
+++ scripts/google-form-submissions-to-mapme-multiple-description-fields.js
@@ -0,0 +1,127 @@
+/**
+ * Base API URL
+ * Set X-API-KEY
+ * Set STORY ID
+ * Set body of mapping categories with ID
+ */
+const baseUrl = 'https://api.mapme.com/graphql'
+const xApiKey = '[editorGeneratedKeyGoesHere]'
+const storyId = '[storyId]'
+const categoriesMapping = {
+    "Category1-name": 'mapme-ID-of-category-1',
+    "Category2-name": 'mapme-ID-of-category-2',
+    "Category3-name": 'mapme-ID-of-category-3',
+}
+
+/**
+ * On form submit event trigger function
+ * @param {object} event - event of form submit
+ */
+function onFormSubmit(event) {
+    const formResponse = event['response']
+    const itemResponses = formResponse.getItemResponses()
+    let name, address, description, categoriesNames, websiteURL, description2, description3
+    let categories = Array()
+    for (let j = 0; j < itemResponses.length; j++) {
+        const itemResponse = itemResponses[j]
+        //   for(let itemResponse in itemResponses){
+        const title = itemResponse.getItem().getTitle()
+        const response = itemResponse.getResponse()
+        switch (title) {
+            case 'Name':
+                name = response
+                break
+            case 'Address':
+                address = response
+                break
+            case 'Description':
+                description = response
+                break
+            case 'Website URL':
+                websiteURL = response
+                break
+            case 'Category':
+                categoriesNames = response
+                categoriesNames.forEach(x => categories.push(`"${categoriesMapping[x]}"`))
+                categories = categories.join(',')
+                break
+			case 'description2':
+                description2 = response
+                break
+			case 'description3':
+                description3 = response
+                break
+        }
+    }
+	const modifiedDesc = "<p>" + description + "</p><p>Description2: " + description2 + "</p><p>description3: " + description3 + "</p>"
+    const body = createRequest(name, address, modifiedDesc, categories, websiteURL)
+    Logger.log(body)
+    const method = 'post'
+    getMapMeApi(method, body)
+}
+/**
+ * Create request body
+ * @param {string} name - name of location
+ * @param {string} address   - address of location
+ * @param {string} description   - description of location
+ * @param {string} categories    - categories of location
+ * @param {string} websiteURL - action button URL
+ * @return {string} body    - body of request
+ */
+function createRequest(name, address, description, categories, websiteURL) {
+    var graphql = JSON.stringify({
+        query: `mutation createSectionMutation {
+        createSection(createSectionInputs: {
+          storyId: "${storyId}"
+          name: "${name}"
+          address: "${address}"
+          description: "${description}"
+          categoryIds: [${categories}]
+          callToAction: {url:"${websiteURL}",title:"More Info"}                        
+        }){
+            id
+            address
+            externalId
+            description
+            mapView{
+              center {
+                lat
+                lng
+              }
+              bearing
+              pitch
+              zoom
+            }
+            callToAction {
+              target
+              url
+              title
+            }
+          }
+        }`,
+    })
+    return graphql
+}
+/**
+ * Base request to MapMe API
+ * @param {string} fullRequest  Full request URL to API
+ * @param {string} method       HTTP method (POST,GET) to API
+ * @param {object} orderObj     Object of request to send to API
+ */
+function getMapMeApi(method = 'get', body = {}) {
+    /** Set request parameters */
+    var options = {
+        contentType: 'application/json',
+        headers: {'X-API-KEY': xApiKey},
+        muteHttpExceptions: true,
+    }
+    if (method === 'post') options.payload = body
+    Logger.log(baseUrl)
+    /** Requset API and return response */
+    let response = UrlFetchApp.fetch(baseUrl, options)
+    Logger.log(response.getResponseCode())
+    Logger.log(response.getContentText())
+    if ([200, 201].indexOf(response.getResponseCode()) == -1) return false
+    Logger.log(response.getContentText())
+    // return JSON.parse(response.getContentText())
+}
